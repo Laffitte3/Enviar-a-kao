ee5d21d4bdaa90a5c80a2d0df5b109bc4bfe9f56
Alejandro <alejandromerlina324@gmail.com>
2024-05-19 09:12:58 -0700

    Agregue las imagenes a una carpeta

--- min.py
+++ min.py
@@ -13,22 +13,22 @@ pantalla= pygame.display.set_mode((ancho,largo))
 pygame.display.set_caption("Salida")
 
 
-fondo= pygame.image.load("fondo.jfif")
+fondo= pygame.image.load("imagenes/fondo.jfif")
 fondo= pygame.transform.scale(fondo,(ancho,largo))
 
-si= pygame.image.load("si.png")
+si= pygame.image.load("imagenes/si.png")
 si_rect= si.get_rect()
 si_rect.center=((ancho/2)-140,(largo/2)+64)
 
-no= pygame.image.load("no.png")
+no= pygame.image.load("imagenes/no.png")
 no_rect= no.get_rect()
 no_rect.center=((ancho/2)+140,(largo/2)+64)
 
-pregunta= pygame.image.load("salirloke.png")
+pregunta= pygame.image.load("imagenes/salirloke.png")
 pregunta= pygame.transform.scale(pregunta,(400,200))
 pregunta_rect= pregunta.get_rect()
 
-cursor= pygame.image.load("cursor.png")
+cursor= pygame.image.load("imagenes/cursor.png")
 cursor_rect= cursor.get_rect()
 cursor_rect.center=(cursor_rect.x,cursor_rect.y)
 
@@ -40,16 +40,13 @@ while running:
     left, middle, right = pygame.mouse.get_pressed()
  
     
-    
-
-
     if cursor_rect.colliderect(si_rect) and left :
 
         pausa = True
 
         while pausa:
 
-            fondo_2= pygame.image.load("lokear.jpg")
+            fondo_2= pygame.image.load("imagenes/lokear.jpg")
             fondo_2= pygame.transform.scale(fondo_2,(ancho,largo))
             pantalla.blit(fondo_2,(0,0))
             pygame.display.update()
@@ -92,6 +89,7 @@ while running:
     pantalla.blit(pregunta,((ancho/2)-200,(largo/2)-200))
     pantalla.blit(no,no_rect)
     pantalla.blit(cursor,cursor_rect)
+    
 
 
     pygame.display.update()
